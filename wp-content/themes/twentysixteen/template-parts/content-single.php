f5a26925b33ef7142082860970e86a9430e261f5
alexwang <2919798881@qq.com>
2019-02-23 11:40:13 +0800

    update ad

--- wp-content/themes/twentysixteen/template-parts/content-single.php
+++ wp-content/themes/twentysixteen/template-parts/content-single.php
@@ -34,7 +34,7 @@
 				get_template_part( 'template-parts/biography' );
 			}
 		?>
-	<div style="width:300px;height:250px;float:left;">
+	<div style="width:50%;min-width:300px;height:250px;float:left;">
 	    <!-- 300_250_1 -->
 	    <ins class="adsbygoogle" style="display:inline-block;width:300px;height:250px" data-ad-client="ca-pub-9477174171188196"
 		data-ad-slot="4142158067"></ins>
@@ -42,7 +42,7 @@
 		(adsbygoogle = window.adsbygoogle || []).push({});
 	    </script>
 	</div>
-	<div style="width:300px;height:250px;float:left;">
+	<div style="width:300px;min-width:300px;height:250px;float:right;">
 	    <!-- 300-250-2 -->
 	    <ins class="adsbygoogle" style="display:inline-block;width:300px;height:250px" data-ad-client="ca-pub-9477174171188196"
 		data-ad-slot="5618891265"></ins>
