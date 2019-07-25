6f78457762914726f405041fcaa261ffc626cf3e
alex <alex@10.82.145.130>
2019-07-25 15:08:23 +0800

    auto commit Thu Jul 25 2019 15:08:22 GMT+0800 (香港標準時間)

---
+++ k8s/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+openssl req -new -x509 -key tls.key -out tls.crt -subj /C=CN/ST=Beijing/L=Beijing/O=DevOps/CN=tomcat.along.com
+kubectl create secret tls tomcat-ingress-secret --cert=tls.crt --key=tls.key
+kubectl create secret tls k7s-dashboard-secret --cert=tls.crt --key=tls.key
+
