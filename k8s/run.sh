d11bb80566bb0939f46154328693682278da0fa9
alex <alex@10.82.145.130>
2019-07-26 12:13:27 +0800

    auto commit Fri Jul 26 2019 12:13:26 GMT+0800 (香港標準時間)

--- k8s/run.sh
+++ k8s/run.sh
@@ -2,4 +2,7 @@
 openssl req -new -x509 -key tls.key -out tls.crt -subj /C=CN/ST=Beijing/L=Beijing/O=DevOps/CN=tomcat.along.com
 kubectl create secret tls tomcat-ingress-secret --cert=tls.crt --key=tls.key
 kubectl create secret tls k7s-dashboard-secret --cert=tls.crt --key=tls.key
+systemctl daemon-reload
+systemctl show --property=Environment docker
+systemctl restart docker
 
