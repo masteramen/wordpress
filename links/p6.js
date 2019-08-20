cb3dbae665ba1061a6dd9d4375d764ca94b8796e
yourname <youremail>
2019-08-20 23:16:31 +0800

    add links for Electron中BrowserWindow对象的所有可设置项 - 简书

--- links/p6.js
+++ links/p6.js
@@ -1,1 +1,1 @@
-{"contents":[{"id":101,"title":"Electron工程踩坑记录 - weixin_34220623的博客 - CSDN博客","link":"https://blog.csdn.net/weixin_34220623/article/details/88029445"}]}
+{"contents":[{"id":102,"title":"Electron中BrowserWindow对象的所有可设置项 - 简书","link":"https://www.jianshu.com/p/0387bd0f8a70?utm_campaign=maleskine&utm_content=note&utm_medium=seo_notes&utm_source=recommendation"},{"id":101,"title":"Electron工程踩坑记录 - weixin_34220623的博客 - CSDN博客","link":"https://blog.csdn.net/weixin_34220623/article/details/88029445"}]}
